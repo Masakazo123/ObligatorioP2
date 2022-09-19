4b9eda23ea72a4d08ce65d9f8e77eb4296376f36
Masakazo123 <54083278+Masakazo123@users.noreply.github.com>
2022-09-19 13:59:19 -0300

    Probando1

--- src/obligatoiro/Obligatoiro.java
+++ src/obligatoiro/Obligatoiro.java
@@ -8,6 +8,8 @@ public class Obligatoiro {
     
     public static void main(String[] args) {
         System.out.println("Prueba");
+        System.out.println("A ver como es la cosa");
+        System.out.println("A ver como es la cosa");
     }
 
 }
